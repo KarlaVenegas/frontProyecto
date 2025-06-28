6b2a41852061d786da56590d1f533a9d162bfa41
EnriqueCons <enri_cons23@outlook.com>
2025-06-27 22:21:58 -0600

    nuevo index

--- src/app/comprador/explorar-cafeterias-comp/explorar-cafeterias-comp.component.ts
+++ src/app/comprador/explorar-cafeterias-comp/explorar-cafeterias-comp.component.ts
@@ -1,36 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Cafeteria } from '../../models/cafeteria';
-import { CafeteriaService } from '../../services/cafeteria.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-explorar-cafeterias-comp',
+  imports: [],
   templateUrl: './explorar-cafeterias-comp.component.html',
-  imports: [CommonModule],
-  styleUrls: ['./explorar-cafeterias-comp.component.css']
+  styleUrl: './explorar-cafeterias-comp.component.css'
 })
-export class ExplorarCafeteriasCompComponent implements OnInit {
-  cafeterias: Cafeteria[] = [];
+export class ExplorarCafeteriasCompComponent {
 
-  constructor(private router: Router, private cafeteriaService: CafeteriaService) {}
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  this.cafeteriaService.getCafeterias().subscribe(
-    (data: Cafeteria[]) => {
-      this.cafeterias = data.sort((a, b) => {
-        // Convierte hora_inicio a minutos para comparar correctamente
-        const [ha, ma] = a.hora_inicio.split(':').map(Number);
-        const [hb, mb] = b.hora_inicio.split(':').map(Number);
-        return ha * 60 + ma - (hb * 60 + mb);
-      });
-    },
-    (error: any) => {
-      console.error('Error fetching cafeterias:', error);
-    }
-  );
-}
-  irAlMenu(id: number) {
-    this.router.navigate(['/comprador/menuCafe', { id: id }]);
+  irAlMenu() {
+    this.router.navigate(['/comprador/menuCafe']);
   }
+
 }
