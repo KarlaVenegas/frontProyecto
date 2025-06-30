e505fbfbc4545135cad7a70e5279ce5ec97d59bf
EnriqueCons <enri_cons23@outlook.com>
2025-06-29 23:04:29 -0600

    login

--- src/app/principal/login/login.component.ts
+++ src/app/principal/login/login.component.ts
@@ -1,32 +1,55 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService, LoginRequest, LoginResponse } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
   imports: [
     ReactiveFormsModule,
-    CommonModule,
-    RouterLink
-  ],
-  styleUrls: ['./login.component.css']
+    CommonModule
+  ]
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  errorMessage = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      correo: ['', [Validators.required, Validators.email]],  // cambiamos el nombre
+      contrasenia: ['', Validators.required]
     });
   }
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      // Lógica de autenticación aquí
-      console.log(this.loginForm.value);
-    }
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    const loginData: LoginRequest = this.loginForm.value;
+
+    this.authService.login(loginData).subscribe({
+      next: (response:LoginResponse) => {
+        // Guarda sesión
+        localStorage.setItem('usuario', JSON.stringify(response));
+
+        // Redirección por tipo
+        if (response.tipo === 'cafeteria') {
+          this.router.navigate(['/cafeteria/']);
+        } else {
+          this.router.navigate(['/comprador/']);
+        }
+      },
+      error: () => {
+        this.errorMessage = 'Correo o contraseña incorrectos';
+      }
+    });
   }
 }
